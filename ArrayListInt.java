9a28f9a98af412ed287e8c312291c96188a4e086
Jesus <jfr1013@gmail.com>
2016-02-26 11:22:51 +0100

    Creada clase ArrayListInt y constructor

---
+++ ArrayListInt.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+/**
+ * Write a description of class ArrayListInt here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ArrayListInt
+{
+    // Array que almacena los números de la lista
+    private int[] listaEnteros;
+    /**
+     * Constructor for objects of class ArrayListInt
+     */
+    public ArrayListInt()
+    {
+        listaEnteros = new int[0];
+    }
+}
