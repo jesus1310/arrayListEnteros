283836a4f7ced361bb2eeaaa425f813d6aff454c
Jesus <jfr1013@gmail.com>
2016-02-26 12:04:15 +0100

    Añadido método add con un elemento y una posicion en la que almacenarlo como parametros

--- ArrayListInt.java
+++ ArrayListInt.java
@@ -28,4 +28,27 @@ public class ArrayListInt
         temp[tamaño] = elemento;
         listaEnteros = temp;
     }
+    
+    /**
+     * Método para añadir elementos a la lista, 
+     * indicando por parámetro en qué posición se almacenarán
+     * Si la posición no es válida no hace nada
+     */
+    public void add(int elemento,int posicion){
+        int tamaño = listaEnteros.length;
+        if ((posicion >= 0) && (posicion < tamaño)){
+            int[] temp = new int[tamaño + 1];
+            
+            int indiceListaEnteros = 0;
+            for (int indiceTemp = 0; tamaño > indiceListaEnteros; indiceTemp++){
+                if (posicion == indiceTemp){
+                    temp[indiceTemp] = elemento;
+                }
+                else {
+                    temp[indiceTemp] = listaEnteros[indiceListaEnteros];
+                    indiceListaEnteros++;
+                }
+            }
+        }
+    }
 }
